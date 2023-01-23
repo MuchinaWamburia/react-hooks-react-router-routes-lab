0360701c2106c45c5c400d854ecfe19c8f1c731e
MuchinaWamburia <lucymuchina20@gmail.com>
2023-01-23 12:27:30 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,57 @@ import Home from "./Home";
 import Movies from "./Movies";
 
 function NavBar() {
-  return <div>
-    <nav>
-    <NavLink exact to="/">Home</NavLink>
-    <NavLink to="/movies">Movies</NavLink>
-    <NavLink to="/directors">Directors</NavLink>
-    <NavLink to ="/actors">Actors</NavLink>
-      </nav>
-  </div>;
-}
+  const linkStyles = {
+    width: "100px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+  return(
+  <div className="navbar">
+     <NavLink
+          to="/"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Home
+      </NavLink>
+      <NavLink
+          to="/movies"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Movies
+      </NavLink> 
+      <NavLink
+          to="/directors"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+         Directors
+      </NavLink>  
+      <NavLink
+          to="/actors"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+         Actors
+      </NavLink> 
+  </div>
+)}
 
 export default NavBar;
