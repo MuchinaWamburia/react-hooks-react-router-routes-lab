cf581d759dcb4a144b01dd9d6347ae18e118891e
MuchinaWamburia <lucymuchina20@gmail.com>
2023-01-23 12:06:09 +0300

    intial

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Directors from "./Directors";
+import Home from "./Home";
+import Movies from "./Movies";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <nav>
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink to="/movies">Movies</NavLink>
+    <NavLink to="/directors">Directors</NavLink>
+    <NavLink to ="/actors">Actors</NavLink>
+      </nav>
+  </div>;
 }
 
 export default NavBar;
